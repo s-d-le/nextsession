e2e19a95ea358a5343c39018785ca05107ca3e87
Son Le <son.le@intersport.com>
2021-09-02 23:15:43 +0200

    main logic is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const LAT = 50.891;
-const LONG = 1.656;
+const LAT = 52.11;
+const LONG = 4.265;
+const KNOT_TO_MS = 1.94384; //Api returns in m/s but we want to work with knots
 
 const App = () => {
   const [lat, setLat] = useState<number>();
   const [long, setLong] = useState<number>();
   const [location, setLocation] = useState<string>("");
+  const [minWindSpeed, setMinWindSpeed] = useState<number>(0);
   const [weather, setWeather] = useState<{}>();
   const fetchURL = `${process.env.REACT_APP_API_URL}/forecast/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`;
+
+  const filterWindSpeed = (list: any[]) => {
+    return list.filter((i) => i.wind.speed >= minWindSpeed / KNOT_TO_MS);
+  };
 
   const getWeather = async () => {
     try {
       const res = await fetch(fetchURL);
       const data = await res.json();
-      setWeather(data);
       setLocation(`${data.city.name}, ${data.city.country}`);
-      console.log(data);
+      console.log(filterWindSpeed(data.list));
     } catch (err) {
       console.log("Error: ", err);
     }
@@ -28,17 +33,35 @@ const App = () => {
     setLong(LONG);
   }, []);
 
+  // Force number input only on windspeed
+  const onInputWindSpeed = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === "" || re.test(e.target.value)) {
+      setMinWindSpeed(e.target.valueAsNumber);
+    }
+  };
+
   return (
     <div className="App">
       <h3>Latitude is: {lat}</h3>
       <h3>Longitude is: {long}</h3>
       <h3>Location is: {location}</h3>
+      <h3>
+        <input
+          type="number"
+          name="windspeed"
+          placeholder="Minimum Wind Speed"
+          value={minWindSpeed}
+          onChange={onInputWindSpeed}
+        />{" "}
+        Knots
+      </h3>
       <button
         onClick={() => {
           getWeather();
         }}
       >
-        Get Weather
+        When is my next session?
       </button>
     </div>
   );
