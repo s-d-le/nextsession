6dc59e74987697637ef8d6b84310bf71bc102615
Son Le <son.le@intersport.com>
2021-10-10 23:55:51 +0200

    call api directly

--- server/resolvers/getWeatherData.ts
+++ server/resolvers/getWeatherData.ts
@@ -1,13 +1,16 @@
 import fetch from "node-fetch";
 type JSONResult = { [key: string]: object };
 
+const https = require("https");
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
 const getWeatherData = async (parent, args, context, info) => {
   const { lat = 0, long = 0 } = args;
-  const url = `${process.env.REACT_APP_WEATHER_API_URL}/forecast/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${long}&units=metric&APPID=db9b795e6127d6da8a42b1d22850ec97`;
   try {
-    const res = await fetch(url, {
-      method: "POST",
-    });
+    const res = await fetch(url, { agent });
     const json = await res.json();
     const currencyObj: JSONResult = json[Object.keys(json)[0]];
     return Object.fromEntries(
